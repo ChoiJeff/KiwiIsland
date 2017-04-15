bc5e0feb6b5900c277339d86b78967dda2760f48
JeffChoi <wndgns0614@gmail.com>
2017-04-15 14:13:29 +1200

    test committion

--- src/nz/ac/aut/ense701/gui/GridSquarePanel.java
+++ src/nz/ac/aut/ense701/gui/GridSquarePanel.java
@@ -3,6 +3,7 @@ package nz.ac.aut.ense701.gui;
 import java.awt.Color;
 import java.util.ArrayList;
 import java.util.List;
+import javax.swing.ImageIcon;
 import javax.swing.border.Border;
 import javax.swing.border.LineBorder;
 import nz.ac.aut.ense701.gameModel.Game;
@@ -29,6 +30,7 @@ public class GridSquarePanel extends javax.swing.JPanel
         this.row    = row;
         this.column = column;
         initComponents();
+        
     }
     
     // the reason to add this method is to access to each panel's location
@@ -52,35 +54,57 @@ public class GridSquarePanel extends javax.swing.JPanel
         boolean squareExplored = game.isExplored(row, column);
         
         Color      color;
-        
+        ImageIcon imgTerrain, imgOccupant = null;
         switch ( terrain )
         {
+            /*
+            case SAND      : imgTerrain = new javax.swing.ImageIcon(getClass().getResource("/nz/ac/aut/ense701/icons/Sand.jpg")) ; break;
+            case FOREST    : imgTerrain = new javax.swing.ImageIcon(getClass().getResource("/nz/ac/aut/ense701/icons/Forrest.png")) ; break;
+            case WETLAND :   imgTerrain = new javax.swing.ImageIcon(getClass().getResource("/nz/ac/aut/ense701/icons/Wetland.JPG")) ;  break;
+            case SCRUB :     imgTerrain = new javax.swing.ImageIcon(getClass().getResource("/nz/ac/aut/ense701/icons/Scrub.JPG")) ; break;
+            case WATER    :  imgTerrain = new javax.swing.ImageIcon(getClass().getResource("/nz/ac/aut/ense701/icons/Water.jpeg")) ; break;
+            default  :       imgTerrain = new javax.swing.ImageIcon(getClass().getResource("/nz/ac/aut/ense701/icons/black.jpg")) ;break;
+            */
+            
             case SAND     : color = Color.YELLOW; break;
             case FOREST   : color = Color.GREEN;  break;
             case WETLAND : color = Color.BLUE; break;
             case SCRUB : color = Color.DARK_GRAY;   break;
             case WATER    : color = Color.CYAN;   break;
             default  : color = Color.LIGHT_GRAY; break;
+                       
         }
         
+        // test submittion
         if ( squareExplored || squareVisible )
         {
             // Set the text of the JLabel according to the occupant
             lblText.setText(game.getOccupantStringRepresentation(row,column));
             
             // change the color of "KC" in order to recognize easily.
-            if(lblText.getText().equals("KC"))
+            if(lblText.getText().equals("KC")){
+                lblText.setText("K");
                 lblText.setForeground(Color.ORANGE);
+            }
+            if(lblText.getText().equals("PKC")){
+                lblText.setText("PK");
+                lblText.setForeground(Color.ORANGE);
+            }
             
+            
+       
             // Set the colour. 
             if ( squareVisible && !squareExplored ) 
             {
                 // When explored the colour is brighter
+                /*
                 color = new Color(Math.min(255, color.getRed()   + 128), 
                                   Math.min(255, color.getGreen() + 128), 
                                   Math.min(255, color.getBlue()  + 128));
+                */
             }
             lblText.setBackground(color);
+            //lblText.setIcon(imgTerrain);
             // set border colour according to 
             // whether the player is in the grid square or not
             setBorder(game.hasPlayer(row,column) ? activeBorder : normalBorder);
@@ -89,6 +113,7 @@ public class GridSquarePanel extends javax.swing.JPanel
         {
             lblText.setText("");
             lblText.setBackground(null);
+            lblText.setIcon(null);
             setBorder(normalBorder);
         }
     }
@@ -116,6 +141,7 @@ public class GridSquarePanel extends javax.swing.JPanel
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel lblText;
     // End of variables declaration//GEN-END:variables
+    
     
     private Game game;
     private int row, column;
