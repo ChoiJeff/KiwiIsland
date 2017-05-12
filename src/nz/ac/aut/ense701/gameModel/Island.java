1edb1bbea89678a4264af3c4b5eb4bd3ec1e5ac1
JeffChoi <wndgns0614@gmail.com>
2017-05-12 18:35:30 +1200

    test commitment

--- src/nz/ac/aut/ense701/gameModel/Island.java
+++ src/nz/ac/aut/ense701/gameModel/Island.java
@@ -307,7 +307,7 @@ public class Island
         }
         return predator;
     }
-      public Kiwi getKiwi(Position position) 
+    public Kiwi getKiwi(Position position) 
     {
         GridSquare square = getGridSquare(position);
         Occupant[] occupants = square.getOccupants();
